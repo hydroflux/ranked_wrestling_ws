bc67cfd6b5b4929accc7080a92bc37d20120e722
Jack Hubert <jphubert93@gmail.com>
2021-10-04 15:52:16 -0600

    re-factored 'frame_handling' to simplify--keeping function names for easy access during regular execution

--- actions/frame_handling.py
+++ actions/frame_handling.py
@@ -3,25 +3,23 @@ from variables.iframes import (header_frame_id, page_frame_id,
                                page_grid_frame_id, search_frame_id)
 
 
-def switch_to_page_frame(browser):
+def switch_to_frame(browser, id, type):
     browser.switch_to.default_content()
-    page_frame = locate_element_by_id(browser, page_frame_id, "page frame")
-    browser.switch_to.frame(page_frame)
+    frame = locate_element_by_id(browser, id, type)
+    browser.switch_to.frame(frame)
+
+
+def switch_to_page_frame(browser):
+    switch_to_frame(browser, page_frame_id, "page frame")
 
 
 def switch_to_page_grid_frame(browser):
-    browser.switch_to.default_content()
-    page_frame = locate_element_by_id(browser, page_grid_frame_id, "page grid frame")
-    browser.switch_to.frame(page_frame)
+    switch_to_frame(browser, page_grid_frame_id, "page grid frame")
 
 
 def switch_to_search_frame(browser):
-    browser.switch_to.default_content()
-    search_frame = locate_element_by_id(browser, search_frame_id, "search frame")
-    browser.switch_to.frame(search_frame)
+    switch_to_frame(browser, search_frame_id, "search frame")
 
 
 def switch_to_header_frame(browser):
-    browser.switch_to.default_content()
-    header_frame = locate_element_by_id(browser, search_frame_id, "header frame")
-    browser.switch_to.frame(header_frame)
+    switch_to_frame(browser, search_frame_id, "header frame")
