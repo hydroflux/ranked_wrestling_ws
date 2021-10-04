5cb1eee182d136d90c48fbf649dcffa3301318e7
Jack Hubert <jphubert93@gmail.com>
2021-10-04 15:26:16 -0600

    re-wrote 'locators' script; created functions to locate elements by tag, name, id, and class

--- selenium_utilities/locators.py
+++ selenium_utilities/locators.py
@@ -8,25 +8,73 @@ from selenium_utilities.locators import locate_elements_by_tag_name
 from settings.timers import timeout
 
 
-def access_iframe_by_tag(browser):
-    iframes = locate_elements_by_tag_name(browser, 'iframe', 'iframes')
-    if len(iframes) == 0:
-        print('Browser unable to locate any iframes on the page, please review.')
-    elif len(iframes) > 1:
-        print('Browser has located multiple iframes on the page, '
-              'please attempt to access using a different method.')
-    else:
-        return iframes[0]
+def print_timeout_statement(type):
+    print(f'Browser timed out trying to locate "{type}", please review.')
 
 
-def locate_iframe_by_name(browser, iframe_name):
+def locate_element_by_id(locator, id, type, clickable=False):
     try:
-        iframe_present = EC.presence_of_element_located((By.NAME, iframe_name))
-        WebDriverWait(browser, timeout).until(iframe_present)
-        iframe = browser.find_element_by_name(iframe_name)
-        return iframe
+        if clickable:
+            element_present = EC.element_to_be_clickable((By.ID, id))
+        else:
+            element_present = EC.presence_of_element_located((By.ID, id))
+        WebDriverWait(locator, timeout).until(element_present)
+        element = locator.find_element_by_id(id)
+        return element
     except TimeoutException:
-        print(f'Browser timed out trying to locate {iframe_name}, please review.')
+        print_timeout_statement(type)
+
+
+def locate_element_by_class_name(locator, class_name, type, clickable=False):
+    try:
+        if clickable:
+            element_present = EC.element_to_be_clickable((By.CLASS_NAME, class_name))
+        else:
+            element_present = EC.presence_of_element_located((By.CLASS_NAME, class_name))
+        WebDriverWait(locator, timeout).until(element_present)
+        element = locator.find_element_by_class_name(class_name)
+        return element
+    except TimeoutException:
+        print_timeout_statement(type)
+
+
+def locate_element_by_name(locator, name, type, clickable=False):
+    try:
+        if clickable:
+            element_present = EC.element_to_be_clickable((By.NAME, name))
+        else:
+            element_present = EC.presence_of_element_located((By.NAME, name))
+        WebDriverWait(locator, timeout).until(element_present)
+        element = locator.find_element_by_name(name)
+        return element
+    except TimeoutException:
+        print_timeout_statement(type)
+
+
+def locate_element_by_tag_name(locator, tag_name, type, clickable=False):
+    try:
+        if clickable:
+            element_present = EC.element_to_be_clickable((By.TAG_NAME, tag_name))
+        else:
+            element_present = EC.presence_of_element_located((By.TAG_NAME, tag_name))
+        WebDriverWait(locator, timeout).until(element_present)
+        element = locator.find_element_by_tag_name(tag_name)
+        return element
+    except TimeoutException:
+        print_timeout_statement(type)
+
+
+def locate_elements_by_tag_name(locator, tag_name, type, clickable=False):
+    try:
+        if clickable:
+            elements_present = EC.element_to_be_clickable((By.TAG_NAME, tag_name))
+        else:
+            elements_present = EC.presence_of_element_located((By.TAG_NAME, tag_name))
+        WebDriverWait(locator, timeout).until(elements_present)
+        elements = locator.find_elements_by_tag_name(tag_name)
+        return elements
+    except TimeoutException:
+        print_timeout_statement(type)
 
 
 def switch_to_default_content(browser):
