c5941b7ba81e802dcb5a30562817c6b5b9b9df6f
Jack Hubert <jphubert93@gmail.com>
2021-10-05 09:41:49 -0600

    created 'pages' script to handle validating information on individual pages

--- actions/frame_handling.py
+++ actions/frame_handling.py
@@ -13,8 +13,8 @@ def switch_to_page_frame(browser):
     switch_to_frame(browser, page_frame_id, "page frame")
 
 
-def switch_to_page_grid_frame(browser):
-    switch_to_frame(browser, page_grid_frame_id, "page grid frame")
+# def switch_to_page_grid_frame(browser):
+#     switch_to_frame(browser, page_grid_frame_id, "page grid frame")
 
 
 def switch_to_search_frame(browser):
---
+++ actions/pages.py
@@ -0,0 +1,9 @@
+from actions.frame_handling import switch_to_page_frame
+from selenium_utilities.locators import locate_element_by_id
+
+from variables.general import main_table_id
+
+
+def get_page_data(browser):
+    switch_to_page_frame(browser)
+    return locate_element_by_id(browser, main_table_id)
