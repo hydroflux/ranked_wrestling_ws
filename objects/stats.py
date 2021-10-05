29d423347ef70e86575d3b8aa1cef456c61a6da5
Jack Hubert <jphubert93@gmail.com>
2021-10-05 08:55:44 -0600

    created a 'stats' object to use during the abstraction

---
+++ objects/stats.py
@@ -0,0 +1,46 @@
+{
+    "league": {
+        "name": [],
+        "city name": [],
+        "city code": [],
+        "state name": [],
+        "division": []
+    },
+    "city": {
+        "dates": [],
+        "start date": [],
+        "end date": [],
+        "name": [],
+        "time": [],
+        "level": [],
+        "type": []
+    },
+    "tournament": {
+        "name": [],
+        "official": [],
+        "team one": [],
+        "team one score": [],
+        "team one points": [],
+        "team two": [],
+        "team two score": [],
+        "team two points": [],
+        "winner": [],
+        "loser": [],
+        "comments": [],
+    },
+    "match": {
+        "weight": []
+        "summary": []
+        "stat flag": []
+        "tw event": []
+        "rounds": []
+        "winner": []
+        "winning team": []
+        "loser": []
+        "losing team": []
+        "results": []
+        "time": []
+        "winning point": []
+        "losing point": []
+    }
+}
