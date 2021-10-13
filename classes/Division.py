23ebea81a5f8c0bab609f08ba43e4006aa595489
Jack Hubert <jphubert93@gmail.com>
2021-10-13 15:00:45 -0600

    changed 'number_of_leagues' and 'number_of_divisions' to 'total_leagues' and 'total_divisions', respectively, to avoid confusion between nested instances of objects

--- classes/Division.py
+++ classes/Division.py
@@ -1,6 +1,6 @@
 class Division:
 
-    number_of_divisions = 0
+    total_divisions = 0
 
     def __init__(self, value, type, state, state_abbreviation, name, division_abbreviation,
                  number_leagues=0, leagues=None
@@ -14,7 +14,7 @@ class Division:
         self.number_leagues = number_leagues
         self.leagues = leagues
 
-        Division.number_of_divisions += 1
+        Division.total_divisions += 1
 
     def __str__(self):
         return f'{self.state} {self.type} - {self.name} ({self.division_abbreviation})'
--- classes/League.py
+++ classes/League.py
@@ -1,6 +1,6 @@
 class League:
 
-    number_of_leagues = 0
+    total_leagues = 0
 
     def __init__(self, name, link, number_teams=0, teams=None):
         self.name = name
@@ -8,7 +8,7 @@ class League:
         self.number_teams = number_teams
         self.teams = teams
 
-        League.number_of_leagues += 1
+        League.total_leagues += 1
 
     def __str__(self):
         return f'{self.name}: {self.number_teams} teams ({self.link})'
