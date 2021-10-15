e1fc03f4e6b79f62097098dde402a5a252e2e97c
Jack Hubert <jphubert93@gmail.com>
2021-10-14 19:16:26 -0600

    unable to figure out how to open links in a new tab instead of an external window; updated events

--- settings/driver.py
+++ settings/driver.py
@@ -7,10 +7,15 @@ def chrome_webdriver(headless):
     options = webdriver.ChromeOptions()
 
     if headless: options.add_argument('--headless')
+    options.add_argument('window-size=1920,1080')
     options.add_argument('--no-sandbox')  # Bypass OS Security Model
     options.add_argument("--disable-infobars")  # https://stackoverflow.com/a/43840128/1689770
     options.add_argument("--disable-browser-side-navigation")  # https://stackoverflow.com/a/49123152/1689770
     options.add_argument('enable-automation')  # https://stackoverflow.com/a/43840128/1689770
+    # options.add_experimental_option("excludeSwitches")
+    # options.add_argument('--disable-extensions')
+    # options.add_argument('--kiosk-printing')
+
 
     driver = webdriver.Chrome(chromedriver, options=options)
     driver.maximize_window()
