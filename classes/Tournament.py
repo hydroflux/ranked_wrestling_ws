c64a2822e50f877e67882fc7f885943adc2f0f6f
Jack Hubert <jphubert93@gmail.com>
2021-10-18 15:16:22 -0600

    updated the Tourament class, additing additional attributes

--- classes/Tournament.py
+++ classes/Tournament.py
@@ -2,9 +2,18 @@ class Tournament:
 
     total_tournaments = 0
 
-    def __init__(self, name, link, number_events=0, events=None):
+    def __init__(self, name, link, official, team_one, team_two,
+                 team_one_score, team_two_score, comment,
+                 number_events=0, events=None
+        ):
         self.name = name
         self.link = link
+        self.official = official
+        self.team_one = team_one
+        self.team_two = team_two
+        self.team_one_score = team_one_score
+        self.team_two_score = team_two_score
+        self.comment = comment
         self.number_events = number_events
         self.events = events
 
