5616baaf9603c84bbd2df1342ba2305feb7216c2
Jack Hubert <jphubert93@gmail.com>
2021-10-20 14:59:48 -0600

    built out 'get_match_summary_information'--working through 'build_match_information'

--- actions/matches.py
+++ actions/matches.py
@@ -1,21 +1,29 @@
 from actions.pages import get_page_data
+from selenium_utilities.locators import locate_element_by_class_name, locate_elements_by_tag_name
 from settings.general_functions import get_direct_link, script_execution
 from settings.printer import iterate_list, print_list_by_index
+
+from variables.general import row_class_name, row_data_tag
 
 
 def build_match_information(match_information):
     pass
 
 
-def get_match_information(browser):
-    match_information = []
-    page_data = get_page_data(browser)
+def get_match_summary_information(browser):
+    match_summary_information = []
+    page_data = get_page_data(browser, False)
+    match_rows = locate_element_by_class_name(page_data, row_class_name, 'match rows')
+    for row in match_rows:
+        match_information = locate_elements_by_tag_name(row, row_data_tag, "match information")
+        match_summary_information.append(build_match_information(match_information))
+    return match_summary_information
 
 
 def add_page_matches(browser, event, match_list):
-    match_information = get_match_information(browser)
-    match_list.extend(match_information)
-    print(f'Added {str(len(match_information))} events to "{event.name}" team list.')
+    match_summary_information = get_match_summary_information(browser)
+    match_list.extend(match_summary_information)
+    print(f'Added {str(len(match_summary_information))} events to "{event.name}" team list.')
 
 
 def add_matches(browser, event, match_list):
