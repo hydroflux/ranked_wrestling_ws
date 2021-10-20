1653bde5060b4bc6134281557a5c55d23f7e571a
Jack Hubert <jphubert93@gmail.com>
2021-10-20 15:11:34 -0600

    created a 'tournaments' action script; added 'tournament_info' to the Match class

--- classes/Match.py
+++ classes/Match.py
@@ -2,12 +2,14 @@ class Match:
 
     total_matches = 0
 
-    def __init__(self, weight, summary, stat_flag, tw_event
+    def __init__(self, weight, summary, stat_flag, tw_event,
+                 tournament_info=None
     ):
         self.weight = weight
         self.summary = summary
         self.stat_flag = stat_flag
         self.tw_event = tw_event
+        self.tournament_info = tournament_info
 
         Match.total_matches += 1
 
