ad2e7db478cade784ec24ce4980d4a5c13d0cde2
Jack Hubert <jphubert93@gmail.com>
2021-10-21 11:27:04 -0600

    created a function to build an Excel writer object using ExcelWriter

--- settings/export.py
+++ settings/export.py
@@ -1,5 +1,5 @@
 import os
-from pandas import DataFrame
+from pandas import DataFrame, ExcelWriter
 
 from settings.settings import target_directory
 
@@ -12,7 +12,24 @@ def create_dataframe(stats):
     return DataFrame([stat.__dict__ for stat in stats])
 
 
+def create_excel_writer(output_file):
+    return ExcelWriter(
+        output_file,
+        engine='xlsxwriter',
+        datetime_format='mm/dd/yyyy',
+        date_format='mm/dd/yyyy')
+
+
+def create_stats_object(target_directory, writer, dataframe):
+    pass
+
+
+def create_xlsx_document(target_directory, file_name, dataframe):
+    pass
+
+
 def export_stats(season, division, stats):
     os.chdir(target_directory)
     file_name = build_file_name(season, division)
     dataframe = create_dataframe(stats)
+    writer = create_xlsx_document(target_directory, file_name, dataframe)
