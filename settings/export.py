c7c8880a1c3385211bd774dc378366ea10663fe5
Jack Hubert <jphubert93@gmail.com>
2021-10-21 11:14:47 -0600

    created a 'create_dataframe' function in 'export'

--- settings/export.py
+++ settings/export.py
@@ -1,4 +1,9 @@
-from pandas import Dataframe
+from pandas import DataFrame
+
+
+def create_dataframe(stats):
+    return DataFrame([stat.__dict__ for stat in stats])
+
 
 def export_stats(stats):
-    pass
+    dataframe = create_dataframe(stats)
