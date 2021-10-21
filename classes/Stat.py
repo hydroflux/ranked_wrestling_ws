3af91e2e1f11062e42ce84eca351d7e17e426df8
Jack Hubert <jphubert93@gmail.com>
2021-10-21 09:04:22 -0600

    updated the stat class printout

--- classes/Stat.py
+++ classes/Stat.py
@@ -47,4 +47,4 @@ class Stat:
         Stat.number_stats += 1
 
     def __str__(self):
-        return f'{self.league} - {self.city} => {self.match}'
+        return f'{self.league_name} - {self.team_name} => {self.summary}'
