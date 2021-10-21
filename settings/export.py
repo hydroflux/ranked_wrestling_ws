f7af5ccdd288b2da2d88d33dc39a4791472c8a12
Jack Hubert <jphubert93@gmail.com>
2021-10-21 13:19:33 -0600

    created a new function to 'set_font_formats' for xlsxwriter & implemented accordingly

--- settings/export.py
+++ settings/export.py
@@ -1,7 +1,7 @@
 import os
 from pandas import DataFrame, ExcelWriter
 
-from variables.export import start_row, title_format, header_format, border_format, last_column, xlsx_columns
+from variables.export import start_row, title_format, header_format, border, border_format, last_column, xlsx_columns
 
 from settings.settings import target_directory
 
@@ -43,6 +43,15 @@ def access_last_row(dataframe):
     return len(dataframe.index) + start_row
 
 
+def set_font_formats(workbook):
+    return {
+        'title': workbook.add_format(title_format['font']),
+        'header': workbook.add_format(header_format['font']),
+        'border_1': workbook.add_format(border_format['1']),
+        'border_2': workbook.add_format(title_format['2'])
+    }
+
+
 def set_page_format(dataframe, worksheet):
     worksheet.set_landscape()
     worksheet.set_paper(5)
@@ -52,19 +61,19 @@ def set_page_format(dataframe, worksheet):
     worksheet.autofilter(f'A{start_row}:{last_column}{access_last_row(dataframe) + 1}')
 
 
-def add_title_row(file_name, dataframe, worksheet):
+def add_title_row(file_name, worksheet, font_formats):
     worksheet.set_row(0, title_format["height"])
-    worksheet.merge_range(f'A1:{last_column}1', file_name, title_format['font'])
+    worksheet.merge_range(f'A1:{last_column}1', file_name, font_formats['title'])
 
 
-def add_headers(dataframe, worksheet):
+def add_headers(dataframe, worksheet, font_formats):
     for index in range(count_columns(dataframe)):
         name = dataframe.columns[index]
         position = xlsx_columns[index]
         worksheet.merge_range(
             f'{position}2:{position}3',
             name,
-            header_format['font']
+            font_formats['header']
         )
 
 
@@ -72,31 +81,30 @@ def access_worksheet_range(dataframe):
     return f'A{start_row}:{last_column}{access_last_row(dataframe)}'
 
 
-def set_border(dataframe, worksheet):
+def set_border(dataframe, worksheet, font_formats):
     worksheet_range = access_worksheet_range(dataframe)
-    worksheet.conditional_format(worksheet_range, border_format['1'])
-    worksheet.conditional_format(worksheet_range, border_format['2'])
+    worksheet.conditional_format(worksheet_range, font_formats['border_1'])
+    worksheet.conditional_format(worksheet_range, font_formats['border_2'])
 
 
-def add_content(file_name, dataframe, worksheet):
-    add_title_row(file_name, dataframe, worksheet)
-    add_headers(dataframe, worksheet)
-    set_border(dataframe, worksheet)
+def add_content(file_name, dataframe, worksheet, font_formats):
+    add_title_row(file_name, worksheet, font_formats)
+    add_headers(dataframe, worksheet, font_formats)
+    set_border(dataframe, worksheet, font_formats)
 
 
 def create_xlsx_document(division, writer, file_name, dataframe):
+    workbook = writer.book
     worksheet = writer.sheets[division.division_abbreviation]
+    font_formats = set_font_formats(workbook)
     set_page_format(dataframe, worksheet)
-    add_content(file_name, dataframe, worksheet)
-
-
-def close_workbook(writer):
-    writer.book.close()
+    add_content(file_name, dataframe, worksheet, font_formats)
+    return workbook
 
 
 def export_stats(season, division, stats):
     os.chdir(target_directory)
     dataframe = create_dataframe(stats)
     writer, file_name = create_stats_object(season, division, dataframe)
-    create_xlsx_document(division, writer, file_name, dataframe)
-    close_workbook(writer)
+    workbook = create_xlsx_document(division, writer, file_name, dataframe)
+    workbook.close()
