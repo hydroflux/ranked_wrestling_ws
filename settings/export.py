fdf2b65453ecd2ab6cb82db09af399d80b16c3d1
Jack Hubert <jphubert93@gmail.com>
2021-10-21 11:49:37 -0600

    started building out a 'set_page_format' function in 'export' & created a 'count_columns' function to return the number of columns in the dataframe

--- settings/export.py
+++ settings/export.py
@@ -1,6 +1,8 @@
 import os
 from pandas import DataFrame, ExcelWriter
 
+from variables.export import start_row
+
 from settings.settings import target_directory
 
 
@@ -26,15 +28,24 @@ def create_stats_object(season, division, dataframe):
     dataframe.to_excel(
         writer,
         sheet_name=division.division_abbreviation,
-        # startrow=,
+        startrow=start_row,
         header=False,
         index=False
     )
     return writer
 
 
+def count_columns(dataframe):
+    return len(dataframe.columns)
+
+
 def set_page_format(worksheet):
-    pass
+    worksheet.set_landscape()
+    worksheet.set_paper(5)
+    worksheet.set_margins(left=0.25, right=0.25, top=0.75, bottom=0.75)
+    worksheet.hide_gridlines(2)
+    worksheet.freeze_panes(f'A{start_row + 1}')
+
 
 
 def format_xlsx_document(workbook, worksheet):
