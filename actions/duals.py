1361479a141ae2ce4cfd8a6e80b05e483286ebe0
Jack Hubert <jphubert93@gmail.com>
2021-12-08 14:22:50 -0700

    updated 'breakdown_dual_match_information' function & its dependents

--- actions/duals.py
+++ actions/duals.py
@@ -2,9 +2,10 @@ from actions.matches import split_match_summary_information
 from classes.Match import Match
 from classes.Stat import Stat
 
-from selenium_utilities.locators import (locate_elements_by_class_name,
+from selenium_utilities.locators import (locate_element_by_tag_name, locate_elements_by_class_name,
                                          locate_elements_by_tag_name)
 
+from variables.duals import official_tag, teams_tag, score_tag
 from variables.general import row_class_name, row_data_tag
 
 from actions.pages import get_page_data
@@ -22,15 +23,30 @@ def build_dual_match_information(match_information):
     return match
 
 
-def breakdown_dual_match_information(page_data):
-    pass
+def breakdown_dual_match_information(match, match_rows):
+    while locate_element_by_tag_name(match_rows[-1], row_data_tag, "last row data").text != '':
+        row_text = match_rows[-1].text
+        if row_text.startswith(official_tag):
+            match.official = row_text[10:]
+        elif row_text.startswith(teams_tag):
+            _, team_one, team_two = row_text.splitlines()
+            match.team_one = team_one.strip()
+            match.team_two = team_two.strip()
+        elif row_text.startswith(score_tag):
+            match.team_one_score = event_results[-1][1]
+            match.team_two_score = event_results[-1][2]
+        else:
+            match.comment = event_results[-1]
+            print(event_results[-1])
+        event_results = event_results[:-1]
+        match_rows.pop()
 
 
 def get_dual_summary_information(browser):
     dual_summary_information = []
     page_data = get_page_data(browser, False)
     match_rows = locate_elements_by_class_name(page_data, row_class_name, 'dual match rows')
-    breakdown_dual_match_information(match_rows)
+    updated_match_rows = breakdown_dual_match_information(match, match_rows)
     for row in match_rows:
         match_information = locate_elements_by_tag_name(row, row_data_tag, 'dual match information')
         dual_summary_information.append(build_dual_match_information(match_information))
